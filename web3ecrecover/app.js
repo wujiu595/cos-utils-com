c8046f63c9a8bea1bdd34da98065a01e974cb904
wujiu595 <676316526@qq.com>
2020-05-30 19:39:25 +0800

    feat:ecrecover signature to pubulic Key

---
+++ web3ecrecover/app.js
@@ -0,0 +1,22 @@
+const ethUtil = require('ethereumjs-util')
+const ethSignUtil = require('eth-sig-util')
+const express = require('express')
+const app = express()
+const port = 80
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+app.post('/ecrecover',jsonParser,
+ (req, res) => {
+   const msgBufferHex = ethUtil.bufferToHex(Buffer.from(req.body.nonce, 'utf8'));
+   const address = ethSignUtil.recoverPersonalSignature({
+     data: msgBufferHex,
+     sig: req.body.signature,
+   });
+   res.send({"publicKey": address.toLowerCase()});
+ }
+)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
